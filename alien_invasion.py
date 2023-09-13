58e108c6971095181675d385e8cad834c9934588
harryhs99 <harry.hstaples@hotmail.com>
2023-09-13 15:52:41 +0100

    add update screen helper method

--- alien_invasion.py
+++ alien_invasion.py
@@ -25,13 +25,7 @@ class AlienInvasion:
         """Start the main loop for the game."""
         while True:
             self._check_events()
-
-            # Redraw the screen during each pass through the loop.
-            self.screen.fill(self.bg_colour)
-            self.ship.blitme()
-
-            # Make the most recently drawn screen visible.
-            pygame.display.flip()
+            self._update_screen()
 
     def _check_events(self):
         """Responds to keypresses and mouse events."""
@@ -39,6 +33,15 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen"""
+        # Redraw the screen during each pass through the loop.
+        self.screen.fill(self.bg_colour)
+        self.ship.blitme()
+
+        # Make the most recently drawn screen visible.
+        pygame.display.flip()
 
 
 if __name__ == '__main__':
