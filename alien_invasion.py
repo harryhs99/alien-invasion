37e15911e6b2f9bb06d643d3304c5523f81da885
harryhs99 <harry.hstaples@hotmail.com>
2023-09-14 12:06:10 +0100

    add alien to game

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 
 class AlienInvasion:
@@ -23,9 +24,13 @@ class AlienInvasion:
             (self.settings.screen_width, self.settings.screen_height))
         # Set caption
         pygame.display.set_caption("Alien Invasion")
-        # Instantiate Ship
+
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+
+        self._create_fleet()
+
         # Set background colour.
         self.bg_colour = (self.settings.bg_colour)
 
@@ -82,6 +87,12 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
 
+    def _create_fleet(self):
+        """Create the fleet of Aliens."""
+        # Make the alien
+        alien = Alien(self)
+        self.aliens.add(alien)
+
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen"""
         # Redraw the screen during each pass through the loop.
@@ -89,6 +100,7 @@ class AlienInvasion:
         self.ship.blitme()
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
+        self.aliens.draw(self.screen)
 
         # Make the most recently drawn screen visible.
         pygame.display.flip()
